02c9e12a7eb3b4da41b2c52dc5cb31bbfdaff80b
wiemag <w.magusiak@gmail.com>
2013-10-01 11:00:27 +0200

    Correction of the '$path' variable determination.

--- jpg2pdf_mono.sh
+++ jpg2pdf_mono.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-VERSION="0.1"
+VERSION="0.2" 		# 2013-10-01, Correction of $path 
 function usage () {
 	echo -e "\nUsage:\n\t\e[1m${0##*/} -c CompressionType filename\e[0m\n"
-	echo -e "Compression type as defined for imagemagick's convert:"
+	echo -e "The 'CompressionType' as defined for imagemagick's convert:"
 	echo -e "\tGroup4 (default), Fax, JPEG, JPEG2000, Lossless"
 	echo -e "\tLZW, RLE, ZIP, BZip, or just None."
-	echo -e "Command '\e[1mconvert -list compress\e[0m' will show a full list."
-	echo -e "of compression types.\n"
+	echo -e "Command \n\t\e[1mconvert -list compress\e[0m"
+	echo -e "will show a full list of compression types.\n"
 }
 CMPR="Group4"		# Compression type
 while getopts "c:hv" flag
@@ -19,9 +19,10 @@ do
 done
 # Remove the options parsed above.
 shift `expr $OPTIND - 1`
-(( $# )) || { echo "Missing file name." ; exit;}
+(( $# )) || { usage; echo -e "\e[31;1mMissing file name.\e[0m" ; exit;}
 
 path=${1%/*}
+[[ "$path" = "$1" ]] && path="."
 f=${1##*/}
 f=${f%.*}
 if [[ -e "$1" ]]; then
